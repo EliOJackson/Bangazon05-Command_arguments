b9bd6d6fb9ca62db7f63f4df41ae9665e99b8715
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-03-05 09:29:51 -0600

    complete and returns 0 if no arguments are passed

--- args.js
+++ args.js
@@ -1,4 +1,11 @@
 const first = process.argv[2];
 const second = process.argv[3];
 
-console.log(`The sum is (${+first + +second})`);
+function letsAdd (first, second) {
+    if (process.argv[2] === undefined) {
+        console.log("0");
+    } else {
+        console.log(`The sum is (${+first + +second})`);
+}};
+
+letsAdd(first, second);
